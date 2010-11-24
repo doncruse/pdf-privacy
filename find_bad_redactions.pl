f9a7ce10ba30353979393d66a612358ffb3c6e26
Timothy B. Lee <tblee@princeton.edu>
2010-11-23 20:24:35 -0500

    Add script

---
+++ find_bad_redactions.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+$debug = 5;
+
+$filename = $ARGV[0];
+
+require "lib/parse_pdf_for_redactions.pl";
+
+open(F, $filename);
+
+my $content;
+
+while(<F>) { $content .= $_; }
+
+my $redactions =  get_bad_redactions($content);
+
+foreach(keys(%$redactions))
+{
+  print "Page $_ : ".$redactions->{$_}."\n";
+}
+
